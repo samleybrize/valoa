3f406947be3739b3b5d7684cf38b04b2b2ae1116
Stephen Berquet <stephen.berquet@gmail.com>
2015-04-23 14:44:00 +0200

    added enum validator

---
+++ src/Samleybrize/Valoa/ValueObject/Validator/ValidatorEnum.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Samleybrize\Valoa\ValueObject\Validator;
+
+use Samleybrize\Valoa\ValueObject\ValueObjectException;
+
+class ValidatorEnum implements ValidatorInterface
+{
+    /**
+     * Min length
+     * @var mixed
+     */
+    private $allowedValues;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $tags = array())
+    {
+        if (!array_key_exists("enum", $tags)) {
+            throw new ValueObjectException("Tag list must have an 'enum' tag");
+        } elseif (!is_array($tags["enum"][0])) {
+            throw new ValueObjectException("Tag 'enum:0' must be an array");
+        }
+
+        $this->allowedValues = $tags["enum"][0];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid(&$value, $strict = false)
+    {
+        if (!in_array($value, $this->allowedValues, true)) {
+            // validation failed
+            $givenType  = ("object" == gettype($value)) ? get_class($value) : gettype($value);
+            $value      = is_scalar($value) ? "[$givenType] '$value'" : "[$givenType]";
+            throw new ValueObjectException("Unexpected value: $value");
+        }
+
+        return true;
+    }
+}
