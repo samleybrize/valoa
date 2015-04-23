bcdf5e6a99db982309e2df286cee738c98b1aa7e
Stephen Berquet <stephen.berquet@gmail.com>
2015-04-23 12:03:31 +0200

    added ip validator

---
+++ src/Samleybrize/Valoa/ValueObject/Validator/ValidatorIp.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Samleybrize\Valoa\ValueObject\Validator;
+
+use Samleybrize\Valoa\ValueObject\ValueObjectException;
+
+class ValidatorIp implements ValidatorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $tags = array())
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid(&$value, $strict = false)
+    {
+        if (false === filter_var($value, FILTER_VALIDATE_IP)) {
+            // validation failed
+            $givenType  = ("object" == gettype($value)) ? get_class($value) : gettype($value);
+            $value      = is_scalar($value) ? "[$givenType] '$value'" : "[$givenType]";
+            throw new ValueObjectException("$value is not a valid IP address");
+        }
+
+        return true;
+    }
+}
