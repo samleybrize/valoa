1dc5f07265f588a817e8f999cd19a8ffd4b32ce3
Stephen Berquet <stephen.berquet@gmail.com>
2015-05-28 17:55:35 +0200

    array validator now handles multi dimensional arrays

--- src/Samleybrize/Valoa/ValueObject/ValueObjectTrait.php
+++ src/Samleybrize/Valoa/ValueObject/ValueObjectTrait.php
@@ -64,70 +64,83 @@ trait ValueObjectTrait
                 continue;
             }
 
-            // TODO recursive type? (eg: string[][])
-            // retrieve main tags
-            $tags       = $docParser->parse($property->getDocComment());
-            $var        = array_key_exists("var", $tags) ? $tags["var"][0] : "any";
-            $validator  = array_key_exists("validator", $tags) ? $tags["validator"][0] : null;
-            $isArray    = false;
-
-            // handle immutable property
-            if (array_key_exists("immutable", $tags)) {
-                $validator = "immutable";
-            }
-
-            // handle array types
-            if ("[]" == substr($var, -2)) {
-                $isArray    = true;
-                $var        = substr($var, 0, -2);
-            } elseif ("array" == $var || "Array" == $var) {
-                $isArray    = true;
-                $var        = "any";
-            }
-
-            // determine required data type
-            if (empty($validator)) {
-                $validator = $var;
-            }
-
-            if ($validator && array_key_exists($validator, self::$valueObjectTypeMapper)) {
-                $validator = self::$valueObjectTypeMapper[$validator];
-            }
-
-            if ($var && array_key_exists($var, self::$valueObjectTypeMapper)) {
-                $var = self::$valueObjectTypeMapper[$var];
-            }
-
-            if (!in_array($var, self::$valueObjectPrimitives) && $validator === $var) {
-                $tags["classname"]  = array($var);
-                $validator          = "object";
-            }
-
-            // instanciate validator
-            $classname      = __NAMESPACE__ . "\\Validator\\Validator" . ucfirst($validator);
-            $builtinExists  = class_exists($classname);
-            $customExists   = class_exists($validator);
-
-            if (!$builtinExists && !$customExists) {
-                throw new ValueObjectException("'$validator' does not name a validator");
-            }
-
+            // create validator
+            $tags                                       = $docParser->parse($property->getDocComment());
             $propertyName                               = $property->getName();
-            $validatorObject                            = $builtinExists ? new $classname($tags) : new $validator($tags);
-
-            if (!$validatorObject instanceof ValidatorInterface) {
-                $class      = get_class($validatorObject);
-                $interface  = __NAMESPACE__ . "\\Validator\\ValidatorInterface";
-                throw new ValueObjectException("Class '$class' does not implements '$interface'");
-            }
-
-            // create array validator
-            if ($isArray) {
-                $validatorObject = new ValidatorArray(array("validator" => $validatorObject));
-            }
-
-            self::$valueObjectValidators[$propertyName] = $validatorObject;
+            self::$valueObjectValidators[$propertyName] = $this->loadValueObjectValidator($tags);
         }
+    }
+
+    /**
+     * Loads a property validator
+     * @param array $tags doc comment tags
+     * @throws \Samleybrize\Valoa\ValueObject\ValueObjectException
+     */
+    private function loadValueObjectValidator(array $tags)
+    {
+        $var            = array_key_exists("var", $tags) ? $tags["var"][0] : "any";
+        $validator      = array_key_exists("validator", $tags) ? $tags["validator"][0] : null;
+        $isArray        = false;
+
+        // handle immutable property
+        if (array_key_exists("immutable", $tags)) {
+            return $this->loadValueObjectValidator(array(
+                "validator" => array("immutable")
+            ));
+        }
+
+        // handle array types
+        if ("[]" == substr($var, -2)) {
+            $isArray    = true;
+            $var        = substr($var, 0, -2);
+        } elseif ("array" == $var || "Array" == $var) {
+            $isArray    = true;
+            $var        = "any";
+        }
+
+        if ($isArray) {
+            $tags["var"][0]     = $var;
+            $validatorObject    = $this->loadValueObjectValidator($tags);
+            $validatorObject    = new ValidatorArray(array("validator" => $validatorObject));
+            return $validatorObject;
+        }
+
+        // determine required data type
+        if (empty($validator)) {
+            $validator = $var;
+        }
+
+        if ($validator && array_key_exists($validator, self::$valueObjectTypeMapper)) {
+            $validator = self::$valueObjectTypeMapper[$validator];
+        }
+
+        if ($var && array_key_exists($var, self::$valueObjectTypeMapper)) {
+            $var = self::$valueObjectTypeMapper[$var];
+        }
+
+        if (!in_array($var, self::$valueObjectPrimitives) && $validator === $var) {
+            $tags["classname"]  = array($var);
+            $validator          = "object";
+        }
+
+        // instanciate validator
+        $classname      = __NAMESPACE__ . "\\Validator\\Validator" . ucfirst($validator);
+        $builtinExists  = class_exists($classname);
+        $customExists   = class_exists($validator);
+
+        if (!$builtinExists && !$customExists) {
+            throw new ValueObjectException("'$validator' does not name a validator");
+        }
+
+        $validatorObject = $builtinExists ? new $classname($tags) : new $validator($tags);
+
+        if (!$validatorObject instanceof ValidatorInterface) {
+            $class      = get_class($validatorObject);
+            $interface  = __NAMESPACE__ . "\\Validator\\ValidatorInterface";
+            throw new ValueObjectException("Class '$class' does not implements '$interface'");
+        }
+
+        return $validatorObject;
     }
 
     /**
