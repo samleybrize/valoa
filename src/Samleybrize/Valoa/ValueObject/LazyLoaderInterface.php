619e52e359a39483aba788eeb8ffde8435d84743
Stephen Berquet <stephen.berquet@gmail.com>
2015-05-29 14:08:16 +0200

    added file license blocks

--- src/Samleybrize/Valoa/ValueObject/LazyLoaderInterface.php
+++ src/Samleybrize/Valoa/ValueObject/LazyLoaderInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Valoa.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Samleybrize\Valoa\ValueObject;
 
 interface LazyLoaderInterface
