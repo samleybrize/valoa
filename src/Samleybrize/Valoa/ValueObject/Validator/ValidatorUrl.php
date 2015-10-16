6a90e990c60a9df41691cf60dbd2b3bbdc438579
Stephen Berquet <stephen.berquet@gmail.com>
2015-10-16 10:45:38 +0200

    added url validator

---
+++ src/Samleybrize/Valoa/ValueObject/Validator/ValidatorUrl.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of Valoa.
+ *
+ * (c) Stephen Berquet <stephen.berquet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Samleybrize\Valoa\ValueObject\Validator;
+
+use Samleybrize\Valoa\ValueObject\ValueObjectException;
+
+class ValidatorUrl implements ValidatorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(array $tags = array())
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isValid(&$value)
+    {
+        if (false === filter_var($value, FILTER_VALIDATE_URL)) {
+            // validation failed
+            $givenType  = ("object" == gettype($value)) ? get_class($value) : gettype($value);
+            $value      = is_scalar($value) ? "[$givenType] '$value'" : "[$givenType]";
+            throw new ValueObjectException("$value is not a valid url");
+        }
+
+        return true;
+    }
+}
